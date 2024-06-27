8d53ca6bd44e93a961cbfb5fa421a4a7ab326b71
sangsongthong <gexterlife.personal@gmail.com>
2024-06-27 08:38:34 +0700

    Step 3 works as intended. Need to adjust the appearance further to reflect exactly as the instruction said.

--- Files/home/coder/project/reactlab/src/components/Card.js
+++ Files/home/coder/project/reactlab/src/components/Card.js
@@ -7,7 +7,16 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <HStack>
+    <VStack textAlign="left">
+      <Image objectFit="cover" src={imageSrc} alt={title} />
+      <Heading as="h2" side="md" textAlign="left">{title}</Heading>
+      <Text>{description}</Text>
+      <FontAwesomeIcon textAlign="left" icon={faArrowRight} size="md"/>
+    </VStack>
+  </HStack>
+  );
 };
 
 export default Card;
